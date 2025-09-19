bbf382c8b3c12d8e81cbe0522c96bc358b8ed6bf
SRI-VARDHAN-ADIKI <2300033389@kluniversity.in>
2025-09-19 17:22:55 +0530

    changed pom.xml and cathenixapplication.java

--- src/main/java/com/rayyan/backend/CalisthenixApplication.java
+++ src/main/java/com/rayyan/backend/CalisthenixApplication.java
@@ -2,12 +2,18 @@ package com.rayyan.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CalisthenixApplication {
+public class CalisthenixApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalisthenixApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalisthenixApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CalisthenixApplication.class);
+    }
 }
